0d34423f936d4df83dfb05cc8fac2a686300cf84
morvoxa <morvoxadev@gmail.com>
2026-08-21 13:47:52 +0700

    add lsp web pnpm

--- src/config/nvim/lua/plugins/lsp.lua
+++ src/config/nvim/lua/plugins/lsp.lua
@@ -7,6 +7,8 @@ return {
         clangd = {},
         lua_ls = {},
         slint_lsp = {},
+        --web
+        --pnpm add -g @tailwindcss/language-server @vtsls/language-server eslint_d emmet-ls vscode-langservers-extracted prettier prettierd
         tailwindcss = {},
         vtsls = {},
         eslint = {},
@@ -14,11 +16,12 @@ return {
         html = {},
         cssls = {},
         jsonls = {},
+        --
         yamlls = {},
         nixd = {},
         pyright = {},
         ruff_lsp = {},
-        taplo= {}
+        taplo = {},
       },
     },
   },
