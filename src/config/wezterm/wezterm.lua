bf93063a9f07e3b328f4164e3facc4262902697c
morvoxa <morvoxadev@gmail.com>
2026-08-29 08:53:21 +0000

    fix

--- src/config/wezterm/wezterm.lua
+++ src/config/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-config.default_prog = { "powershell.exe", "-NoLogo" }
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" or wezterm.target_triple:find("windows") then
+	config.default_prog = { "powershell.exe", "-NoLogo" }
+else
+	config.default_prog = { os.getenv("SHELL") or "bash" }
+end
+
 config.keys = {
 	{
 		key = "L",
@@ -13,5 +19,16 @@ config.keys = {
 		action = wezterm.action.ActivateTabRelative(-1),
 	},
 }
+
 config.font = wezterm.font("JetBrainsMono Nerd Font")
+
+config.window_decorations = "NONE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 return config
