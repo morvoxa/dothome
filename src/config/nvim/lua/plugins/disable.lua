29c17bff4c41d39dd8fcd8c8229390fb72a5c758
morvoxa <morvoxadev@gmail.com>
2026-08-31 23:35:22 -0700

    [new] nvimts manager

--- src/config/nvim/lua/plugins/disable.lua
+++ src/config/nvim/lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
   { "mason-org/mason.nvim", enabled = false },
   { "mason-org/mason-lspconfig.nvim", enabled = false },
   { "mason-org/mason-tool-installer.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
 }
