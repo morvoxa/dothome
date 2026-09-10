18223b3ab30baa891f98c681ef71fd2727f316b5
morvoxa <morvoxadev@gmail.com>
2026-09-09 22:38:17 -0700

    [alacritty]: add config

--- windows.ps1
+++ windows.ps1
@@ -1,6 +1,7 @@
 $Configs = @(
     @{ Name = "Code"; Source = "src/config/Code"; Destination = "$HOME\AppData\Roaming\Code"; IsFolder = $true }
     @{ Name = "neovim"; Source = "src/config/nvim"; Destination = "$HOME\AppData\Local\nvim"; IsFolder = $true }
+    @{ Name = "Alacritty"; Source = "src/config/alacritty"; Destination = "$HOME\AppData\Roaming\alacritty"; IsFolder = $true }
     @{ Name = "Profile"; Source = "src/WindowsPowerShell"; Destination = "$HOME\Documents\WindowsPowerShell"; IsFolder = $true }
     @{ Name = "GitConfig"; Source = "src/config/gitconfig"; Destination = "$HOME\.gitconfig"; IsFolder = $false }
 )
