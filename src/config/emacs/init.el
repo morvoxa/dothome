9e52d784e7a604b623eafe180c2232d7f5e45edc
morvoxa <morvoxadev@gmail.com>
2026-09-21 17:04:02 +0700

    [emacs]: stable

--- src/config/emacs/init.el
+++ src/config/emacs/init.el
@@ -7,21 +7,32 @@
 ;; font fix
 (set-face-attribute 'default nil :font "JetBrains Mono Nerd Font" :height 100)
 ;; evil fix fix
+(setq-default tab-width 2)          ; Lebar tampilan karakter tab
+(setq-default evil-shift-width 2)   ; Jarak geser saat pakai << atau >>
+(setq-default standard-indent 2)
+
 (defun my-unified-find-file ()
   (interactive)
   (let ((locked-dir default-directory))
     (call-interactively 'find-file)
     (setq default-directory locked-dir)))
+
+
 (use-package evil
   :ensure t
   :init
   (evil-mode 1)
   :config
   (with-eval-after-load 'evil
+
     (define-key evil-insert-state-map (kbd "C-y") nil)
     (define-key evil-insert-state-map (kbd "C-n") nil)
     (define-key evil-insert-state-map (kbd "C-p") nil)
     (evil-set-leader 'normal (kbd "SPC"))
+    (define-key evil-insert-state-map (kbd "TAB") 
+								(lambda () 
+									(interactive)
+									(execute-kbd-macro (kbd "M-i"))))
     (define-key evil-normal-state-map (kbd "<leader> f") 'my-unified-find-file)
     (define-key evil-normal-state-map (kbd "<leader> o") 'cd)
     (define-key evil-normal-state-map (kbd "<leader> r") 'eval-buffer)
@@ -30,10 +41,36 @@
     (define-key evil-normal-state-map (kbd "<leader> e") (lambda () (interactive) (find-file "."))))
   (define-key evil-normal-state-map (kbd "C-i") 'scroll-down-command)
   (define-key evil-normal-state-map (kbd "<tab>") 'indent-for-tab-command)
-  (evil-define-key 'normal 'global (kbd "<leader> x") 'kill-current-buffer)
-  (evil-define-key 'normal 'global (kbd "<leader> q") 'kill-emacs)
+  (evil-define-key 'normal 'global (kbd "<leader> x") 'kill-current-buffer )
+  (evil-define-key 'normal 'global (kbd "<leader> q") 'about-emacs)
+  ;;write
   (evil-define-key 'normal 'global (kbd "<leader> w") 'save-buffer)
   (evil-define-key 'normal 'global (kbd "<leader> v") 'other-window))
+
+
+
+(require 'dired)
+(put 'dired-find-alternate-file 'disabled nil)
+
+(with-eval-after-load 'dired
+  ;; dired map
+  (define-key dired-mode-map (kbd "a")  #'dired-create-empty-file)
+  (define-key dired-mode-map (kbd "y") #'dired-copy-filename-as-kill)
+  (define-key dired-mode-map (kbd "d") #'dired-do-flag-delete)
+  (define-key dired-mode-map (kbd "u")  #'dired-unmark)
+  
+  (when (bound-and-true-p evil-mode)
+    (evil-define-key 'normal dired-mode-map 
+      (kbd "h") 'dired-up-directory
+      (kbd "l") 'dired-find-alternate-file)))
+;;; auto remove dired buffer
+(defun my-dired-reuse-buffer-advice (orig-fun &rest args)
+  (if (file-directory-p (dired-get-file-for-visit))
+      (dired-find-alternate-file)
+    (apply orig-fun args)))
+
+
+
 
 ;; j k ke normal mode
 (use-package key-chord
@@ -52,6 +89,41 @@
 (ido-everywhere 1)
 (setq ido-enable-flex-matching t)
 (setq ido-use-virtual-buffers t)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package avy
+  :ensure t
+  :bind
+  (:map evil-normal-state-map
+        ("s" . avy-goto-char-2)))
+;;formtter
+(use-package apheleia
+  :ensure t
+  :init
+  (apheleia-global-mode 1))
+;;tree sitter
+(use-package treesit-auto
+  :ensure t
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+;; auto pairs
+(setq electric-pair-pairs
+      '(
+        (?\{ . ?\})
+        (?\' . ?\')
+        (?\` . ?\`)
+        (?\< . ?\>)
+        ))
+(electric-pair-mode 1)
+
+
 ;;(use-package ivy
 ;;:ensure t
 ;;:diminish
@@ -83,82 +155,47 @@
 ;;(ivy-rich-mode 1))
 
 ;;avy
-(use-package avy
-  :ensure t
-  :bind
-  (:map evil-normal-state-map
-        ("s" . avy-goto-char-2)))
-;;formtter
-(use-package apheleia
-  :ensure t
-  :init
-  (apheleia-global-mode 1))
-;;tree sitter
-(use-package treesit-auto
-  :ensure t
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
-;; auto pairs
-(setq electric-pair-pairs
-      '(
-        (?\{ . ?\})
-        (?\' . ?\')
-        (?\` . ?\`)
-        (?\< . ?\>)
-        ))
-(electric-pair-mode 1)
-;; completion lsp
-(use-package corfu
-  :ensure t
-  :custom
-  (corfu-auto t)
-  (corfu-cycle t)
-  (corfu-auto-delay 0.0)      ; Instan tanpa jeda layaknya VS Code
-  (corfu-auto-prefix 1)       ; Ketik 1 huruf langsung memicu popup
-  :bind (:map corfu-map
-              ("C-n" . corfu-next)
-              ("C-p" . corfu-previous)
-              ("C-y" . corfu-insert))
-  :init
-  (global-corfu-mode)
-  (corfu-popupinfo-mode 1))
-
-(unless (package-installed-p 'yasnippet)
-  (package-install 'yasnippet))
-(require 'yasnippet)
-(yas-global-mode 1)
-
-(use-package eglot
-  :ensure nil
-  :config
-  ;; Gunakan setq murni untuk keamanan jalur server
-  (setq eglot-server-programs
-        '((rust-mode . ("rust-analyzer"))
-          (typescript-mode . ("vtsls" "--stdio"))
-          (tsx-ts-mode . ("vtsls" "--stdio"))
-          (js-ts-mode . ("vtsls" "--stdio"))
-	  ;;          (html-ts-mode . ("emmet-ls" "--stdio"))
-          ((c-mode c++-mode c-ts-mode c++-ts-mode) . ("clangd"))))
-
-  ;; Jembatan filter Corfu agar Eglot tidak menyembunyikan data completion
-  (setq completion-category-overrides '((eglot (styles basic substring)))))
-
-;; =====================================================================
-;; FORCE AUTO-START EGLOT
-;; =====================================================================
-(add-hook 'c-mode-hook #'eglot-ensure)
-(add-hook 'c++-mode-hook #'eglot-ensure)
-(add-hook 'c-ts-mode-hook #'eglot-ensure)
-(add-hook 'c++-ts-mode-hook #'eglot-ensure)
-(add-hook 'tsx-ts-mode-hook #'eglot-ensure)
-(add-hook 'js-ts-mode-hook #'eglot-ensure)
-(add-hook 'typescript-mode-hook #'eglot-ensure)
-;;shell exec
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (exec-path-from-shell-initialize))
-
+;;;; completion lsp
+;;(use-package corfu
+;;:ensure t
+;;:custom
+;;(corfu-auto t)
+;;(corfu-cycle t)
+;;(corfu-auto-delay 0.0)      ; Instan tanpa jeda layaknya VS Code
+;;(corfu-auto-prefix 1)       ; Ketik 1 huruf langsung memicu popup
+;;:bind (:map corfu-map
+;;("C-n" . corfu-next)
+;;("C-p" . corfu-previous)
+;;("C-y" . corfu-insert))
+;;:init
+;;(global-corfu-mode)
+;;(corfu-popupinfo-mode 1))
+;;
+;;(unless (package-installed-p 'yasnippet)
+;;(package-install 'yasnippet))
+;;(require 'yasnippet)
+;;(yas-global-mode 1)
+;;
+;;(use-package eglot
+;;:ensure nil
+;;:config
+  ;;;; Gunakan setq murni untuk keamanan jalur server
+;;(setq eglot-server-programs
+;;'((rust-mode . ("rust-analyzer"))
+;;((typescript-ts-mode tsx-ts-mode ) . ("vtsls" "--stdio"))
+;;((  c-ts-mode c++-ts-mode) . ("clangd"))))
+;;
+  ;;;; Jembatan filter Corfu agar Eglot tidak menyembunyikan data completion
+;;(setq completion-category-overrides '((eglot (styles basic substring)))))
+;;
+;;;; =====================================================================
+;;;; FORCE AUTO-START EGLOT
+;;;; =====================================================================
+;;;; Daftarkan semua mode sekaligus dalam sebuah list
+;;(dolist (hook '(c-ts-mode-hook
+;;typescript-ts-mode-hook
+;;rust-mode-hook
+;;tsx-ts-mode-hook
+;;c++-ts-mode-hook))
+;;(add-hook hook #'eglot-ensure))
+;;;;shell exec
