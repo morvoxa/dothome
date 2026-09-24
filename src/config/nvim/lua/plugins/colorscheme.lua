c7f1858fb2820bdd07873b94332bd5096ec4652e
morvoxa <morvoxadev@gmail.com>
2026-09-24 13:49:32 +0700

    [neovim]: remove colorscheme

--- src/config/nvim/lua/plugins/colorscheme.lua
+++ src/config/nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,3 @@ vim.pack.add({
 	{ src = "https://github.com/folke/tokyonight.nvim" },
 })
 vim.cmd("colorscheme tokyonight")
-local transparent_groups = {
-	"Normal",
-	"SignColumn",
-}
-
-for _, group in ipairs(transparent_groups) do
-	vim.api.nvim_set_hl(0, group, { bg = "none" })
-end
