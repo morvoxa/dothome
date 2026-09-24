d36a7be81ee2a2bf4cf7b2207dedc5ae5f08fb0d
morvoxa <morvoxadev@gmail.com>
2026-09-24 16:01:39 +0700

    [neovim]: for svelte

--- src/config/nvim/lua/plugins/conform.lua
+++ src/config/nvim/lua/plugins/conform.lua
@@ -1,4 +1,13 @@
 require("conform").setup({
+	formatters = {
+		oxfmt = {
+			command = function()
+				local cwd = vim.uv.cwd() or vim.fn.getcwd()
+				local local_bin = cwd .. "/node_modules/.bin/oxfmt"
+				return local_bin
+			end,
+		},
+	},
 	formatters_by_ft = {
 		lua = { "stylua" },
 		toml = { "taplo" },
