65dfcebf6078a9f65075227107950ee174862e45
morvoxa <morvoxadev@gmail.com>
2026-09-25 12:57:54 +0700

    [add]: config kitty and transparent neovim

--- src/config/nvim/lua/plugins/colorscheme.lua
+++ src/config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
 vim.pack.add({
 	{ src = "https://github.com/folke/tokyonight.nvim" },
 })
+require("tokyonight").setup({
+	transparent = true, -- Enable transparency for the main editor window
+	styles = {
+		sidebars = "transparent", -- Clear background for sidebars
+		floats = "transparent", -- Clear background for floating windows
+	},
+})
 vim.cmd("colorscheme tokyonight")
