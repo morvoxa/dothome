11e099de86bbb94e1fd6be810a8fb2ca6e296c75
morvoxa <morvoxadev@gmail.com>
2026-09-25 16:13:17 +0700

    [neovim]: svelte

--- src/config/nvim/lua/plugins/snippet.lua
+++ src/config/nvim/lua/plugins/snippet.lua
@@ -1,8 +1,12 @@
 vim.pack.add({
 	{ src = "https://github.com/rafamadriz/friendly-snippets" },
 	{ src = "https://github.com/L3MON4D3/LuaSnip" },
+	{ src = "https://github.com/nvim-svelte/nvim-svelte-snippets" },
 })
 -- will exclude all javascript snippets
 require("luasnip.loaders.from_vscode").load({
 	--	exclude = { "javascript" },
+})
+require("nvim-svelte-snippets").setup({
+	auto_detect = true, -- Only load in SvelteKit projects
 })
